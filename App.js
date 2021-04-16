9498bdbbeef7129d00f4771b9027ccbe3829ede9
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2021-04-16 23:41:30 +0200

    funcionando con fazt

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions } from "react-native";
+import DanielLogo from './assets/img/daniel-logo.png';
 
-export default function App() {
+const App = () => {
+
+  const ponerNombre = () => {
+
+    console.log('Daniel');
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
+    <View style={styles.container}>
+      <Image 
+      
+      style={styles.image} 
+      source={DanielLogo}
+      
+      />
+
+      <TouchableOpacity 
+        onPress={ponerNombre}
+        style={styles.button}
+      >
+
+        <Text>
+          Mucho gusto!
+        </Text>
+
+      </TouchableOpacity>
+
+    </View>  
+  );
+};
+
+
+const styles = StyleSheet.create ({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  title: {
+    color: 'black',
+    marginTop: 20
+  },
+  image: {
+    width: 160,
+    height: 133,
+
+  },
+  buttonBox: {
+    marginTop: 20,
+    width: 300, 
+    height: 60,
+    borderWidth: 2,
+    borderColor: 'black'
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 45,
+    borderColor: 'black',
+    borderWidth: 2,
+    marginTop: 40
+  }
+})
+
+export default App;
